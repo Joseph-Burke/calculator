113b9318dc74266cb8016f7a2f61ea6ce0b7937d
Joseph Burke <joseph_burke@outlook.com>
2020-12-21 23:28:33 +0000

    Implement Button and ButtonPanel components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,2 +1,22 @@
+import React, { Component } from 'react';
+
 import PropTypes from "prop-types";
 import Big from "big.js";
+
+export default class Button extends Component {
+  constructor(props) {
+    super(props)
+  };
+
+  render() {
+    return (
+      <button>
+        {this.props.name}
+      </button>
+    )
+  }
+}
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired
+};
