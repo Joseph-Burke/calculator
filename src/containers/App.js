d269d2bca87356d74c9eac4eb0210503fe3d236d
Joseph Burke <joseph_burke@outlook.com>
2020-12-24 15:48:02 +0000

    Remove import statement from App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from "prop-types";
-import Big from 'big.js';
 import Display from '../components/Display';
 import ButtonPanel from '../components/ButtonPanel';
 import calculate from '../logic/calculate';
