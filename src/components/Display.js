56a17bc11284330f30794f1322dba5dd3a861b06
Joseph Burke <joseph_burke@outlook.com>
2020-12-27 16:59:00 +0000

    Remove redundant import, propTypes and comment

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,16 +1,9 @@
-// import React, { Component } from 'react';
-import PropTypes from "prop-types";
-
 const Display = props => {
   return <div>{props.result}</div>;
 }
 
 Display.defaultProps = {
   result: '0',
-};
-
-Display.propTypes = {
-  result: PropTypes.string.isRequired,
 };
 
 export default Display;
