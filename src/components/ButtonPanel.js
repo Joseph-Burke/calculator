2f9046fe1b78c6d04238ecbdc88183c4f82eb5bd
Joseph Burke <joseph_burke@outlook.com>
2020-12-27 13:10:55 +0000

    Refactor ButtonPanel to be a functional component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,52 +1,47 @@
-import { Component } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Button from "./Button";
 
-export default class ButtonPanel extends Component {
-  constructor(props) {
-    super(props)
-  };
-
-  render() {
-    const data = this.props.data;
-    const processInput = this.props.inputHandler;
-    return (
+const ButtonPanel = props => {
+  const data = props.data;
+  const processInput = props.inputHandler;
+  return (
+    <div>
       <div>
-        <div>
-          <Button name={"AC"} data={data} processInput = {processInput} />
-          <Button name={"+/-"} data={data} processInput = {processInput} />
-          <Button name={"%"} data={data} processInput = {processInput} />
-          <Button name={"÷"} data={data} processInput = {processInput} />
-        </div>
-        <div>
-          <Button name={"7"} data={data} processInput = {processInput} />
-          <Button name={"8"} data={data} processInput = {processInput} />
-          <Button name={"9"} data={data} processInput = {processInput} />
-          <Button name={"X"} data={data} processInput = {processInput} />
-        </div>
-        <div>
-          <Button name={"4"} data={data} processInput = {processInput} />
-          <Button name={"5"} data={data} processInput = {processInput} />
-          <Button name={"6"} data={data} processInput = {processInput} />
-          <Button name={"-"} data={data} processInput = {processInput} />
-        </div>
-        <div>
-          <Button name={"1"} data={data} processInput = {processInput} />
-          <Button name={"2"} data={data} processInput = {processInput} />
-          <Button name={"3"} data={data} processInput = {processInput} />
-          <Button name={"+"} data={data} processInput = {processInput} />
-        </div>
-        <div>
-          <Button name={"0"} data={data} processInput = {processInput} />
-          <Button name={"."} data={data} processInput = {processInput} />
-          <Button name={"="} data={data} processInput = {processInput} />
-        </div >
+        <Button name={"AC"} data={data} processInput={processInput} />
+        <Button name={"+/-"} data={data} processInput={processInput} />
+        <Button name={"%"} data={data} processInput={processInput} />
+        <Button name={"÷"} data={data} processInput={processInput} />
       </div>
-    )
-  };
-}
+      <div>
+        <Button name={"7"} data={data} processInput={processInput} />
+        <Button name={"8"} data={data} processInput={processInput} />
+        <Button name={"9"} data={data} processInput={processInput} />
+        <Button name={"X"} data={data} processInput={processInput} />
+      </div>
+      <div>
+        <Button name={"4"} data={data} processInput={processInput} />
+        <Button name={"5"} data={data} processInput={processInput} />
+        <Button name={"6"} data={data} processInput={processInput} />
+        <Button name={"-"} data={data} processInput={processInput} />
+      </div>
+      <div>
+        <Button name={"1"} data={data} processInput={processInput} />
+        <Button name={"2"} data={data} processInput={processInput} />
+        <Button name={"3"} data={data} processInput={processInput} />
+        <Button name={"+"} data={data} processInput={processInput} />
+      </div>
+      <div>
+        <Button name={"0"} data={data} processInput={processInput} />
+        <Button name={"."} data={data} processInput={processInput} />
+        <Button name={"="} data={data} processInput={processInput} />
+      </div>
+    </div>
+  );
+};
 
 ButtonPanel.propTypes = {
   data: PropTypes.object.isRequired,
   inputHandler: PropTypes.func.isRequired
 };
+
+export default ButtonPanel;
