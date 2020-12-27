d569e34b97c16ff028824d76db984d78b534c891
Joseph Burke <joseph_burke@outlook.com>
2020-12-27 20:25:47 +0000

    Fix console error

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 const Display = ({ result }) => <div>{result}</div>;
 
 Display.propTypes = {
-  result: PropTypes.string.isRequired,
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
 };
 
 export default Display;
